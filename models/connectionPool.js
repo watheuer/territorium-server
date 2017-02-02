3462ef5603ba2928d947054dbebef276817818cb
Will Theuer <watheuer@email.wm.edu>
2017-02-02 14:53:35 -0500

    Fix env variables for postgres

--- models/connectionPool.js
+++ models/connectionPool.js
@@ -1,11 +1,15 @@
 var pg = require('pg');
+var url = require('url');
+
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(':');
 
 var config = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
   max: process.env.DB_MAX_CONNECTIONS,
   idleTimeoutMillis: 30000
 };
--- sample_env.sh
+++ sample_env.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-# these env variables are needed, rename to env_DEV.sh and env_TEST.sh
 export PORT=3000
 
-export DB_USER=
-export DB_PASSWORD=
-export DB_HOST=localhost
-export DB_PORT=5432
-export DB_DATABASE=
+export DATABASE_URL=
 export DB_MAX_CONNECTIONS=10
 
 export JWT_SECRET=
